098522c8bea0b27a4b21981620d2db7e42ab4012
tahasozgen <sozgentaha70@gmail.com>
2018-10-09 10:07:48 +0300

    Add project files.

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
